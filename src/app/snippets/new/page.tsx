2687823f2649cce28b03b8ff561f1e9d8c6e2c76
hnin <hnin0929@ecplaza.net>
2024-03-05 11:02:20 +0900

    useform state in action

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,35 +1,14 @@
+'use client';
 
-import { redirect } from 'next/navigation';
-import { db } from '@/db';
+import { useFormState } from "react-dom";
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
-
-    async function createSnippet(formData: FormData) {
-        // This needs to be a server action!
-        'use server';
-
-        // Check the user's inputs and make sure they're valid
-        const title = formData.get('title') as string;
-        const code = formData.get('code') as string;
-
-        // Create a new record in the database
-        const snippet = await db.snippet.create({
-            data: {
-                title,
-                code
-            }
-        });
-        console.log(snippet);
-
-        // Redirect the user back to the root route
-        redirect('/');
-
-    }
-
+    const [formState, action] = useFormState(actions.createSnippet, { message : ''});
 
     return (
         <>
-        <form action={createSnippet}>
+        <form action={action}>
             <h3 className="font-bold m-3">Create a Snippet</h3>
             <div className="flex flex-col gap-4">
 
@@ -54,6 +33,8 @@ export default function SnippetCreatePage() {
                         id="code"
                     />
                 </div>
+
+                <div>{formState.message}</div>
 
                 <button type="submit" className="rounded p-2 bg-blue-200">
                     Create
