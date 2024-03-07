a13c45c53aa65c1cce9c187d88ac8ffcecf8afe0
hnin <hnin0929@ecplaza.net>
2024-03-07 15:48:55 +0900

    gotchas around error handling in nextjs

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -25,30 +25,43 @@ export async function createSnippet(
     formData: FormData
     ) {
     
-    // Check the user's inputs and make sure they're valid
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
+    try{
+         // Check the user's inputs and make sure they're valid
+        const title = formData.get('title') as string;
+        const code = formData.get('code') as string;
 
-    if(typeof title != 'string' || title.length < 3){
-        return {
-            message: 'Title must be longer'
-        };
-    }
-
-    if(typeof code != 'string' || code.length < 10){
-        return {
-            message: 'Code must be longer'
-        };
-    }
-
-    // Create a new record in the database
-    const snippet = await db.snippet.create({
-        data: {
-            title,
-            code
+        if(typeof title != 'string' || title.length < 3){
+            return {
+                message: 'Title must be longer'
+            };
         }
-    });
-    console.log(snippet);
+
+        if(typeof code != 'string' || code.length < 10){
+            return {
+                message: 'Code must be longer'
+            };
+        }
+
+        // Create a new record in the database
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        });
+
+    }catch (err: unknown){
+        if(err instanceof Error){
+            return {
+                message: err.message
+            };
+        }else{
+            return {
+                message: 'Something went wrong...'
+            };
+        }
+    }
+   
 
     // Redirect the user back to the root route
     redirect('/');
